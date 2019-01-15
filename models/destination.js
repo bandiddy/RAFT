6df018ea32bfd37acfa6aaa976a9ea8506626820
Calum Ragan <searagan@gmail.com>
2019-01-15 13:21:43 -0800

    survey db set up

--- models/destination.js
+++ models/destination.js
@@ -1,7 +1,32 @@
-module.exports = function(sequelize, DataTypes) {
+  module.exports = function(sequelize, DataTypes) {
     var Destination = sequelize.define("Destination", {
-      
+      country: {
+        type: DataTypes.STRING
+      },
+      climate: {
+        type: DataTypes.STRING
+      },
+      setting: {
+        type: DataTypes.BOOLEAN
+      },
+      crowds: {
+        type: DataTypes.BOOLEAN
+      },
+      food: {
+        type: DataTypes.BOOLEAN
+      },
+      travler: {
+        type: DataTypes.BOOLEAN
+      },
+      outdoor: {
+        type: DataTypes.BOOLEAN
+      },
+      museums: {
+        type: DataTypes.BOOLEAN
+      }
     });
+
     return Destination;
   };
   
+
