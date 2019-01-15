a2b385d8e83f2baf091fc9b679429e99fed40e0c
James <james.je.cho@gmail.com>
2019-01-15 11:48:48 -0800

    created login functionality

--- models/destination.js
+++ models/destination.js
@@ -1,7 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
     var Destination = sequelize.define("Destination", {
-      
+      country: {
+        type: DataTypes.STRING
+      },
+      weather: {
+        type: DataTypes.STRING
+      },
+      style: {
+        type: DataTypes.STRING
+      },
+      crowded: {
+        type: DataTypes.BOOLEAN
+      },
+      category: {
+        type: DataTypes.STRING
+      } 
     });
+
     return Destination;
   };
   
