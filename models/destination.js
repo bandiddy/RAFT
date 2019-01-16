97c8d8112d65ac371f7ffccc0491767220fe0e25
alvin <alvinjoseph.garcia@gmail.com>
2019-01-16 11:02:19 -0800

    new new

--- models/destination.js
+++ models/destination.js
@@ -1,41 +1,24 @@
-  module.exports = function(sequelize, DataTypes) {
-    var Destination = sequelize.define("Destination", {
-      name: {
-        type: DataTypes.STRING
-      },
-      country: {
-        type: DataTypes.STRING
-      },
-      category: {
-        type: DataTypes.BOOLEAN
-      },
-      bestSeason: {
-        type: DataTypes.STRING
-      },
-      crowded: {
-        type: DataTypes.BOOLEAN
-      },
-      food: {
-        type: DataTypes.BOOLEAN
-      },
-      tourism: {
-        type: DataTypes.BOOLEAN
-      },
-      outdoor: {
-        type: DataTypes.BOOLEAN
-      },
-      climate: {
-        type: DataTypes.STRING
+module.exports = function (sequelize, DataTypes) {
+  var Destination = sequelize.define("Destination", {
+    name: {
+      type: DataTypes.STRING
+    },
+    country: {
+      type: DataTypes.STRING
+    },
+    climate: {
+      type: DataTypes.STRING
+    },
+    bestSeason: {
+      type: DataTypes.STRING
+    }
+  });
+  Destination.associate = function (models) {
+    Destination.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
       }
     });
-    Destination.associate = function(models) {
-      Destination.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-    return Destination;
   };
-  
-
+  return Destination;
+};
