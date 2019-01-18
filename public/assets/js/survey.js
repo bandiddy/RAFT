0d0c67389e24b22833d8a63848ac00907364ce07
alvin <alvinjoseph.garcia@gmail.com>
2019-01-18 10:26:40 -0800

    new

--- public/assets/js/survey.js
+++ public/assets/js/survey.js
@@ -6,12 +6,8 @@ $("#submit").on("click", function () {
     console.log($("#q1").val())
     function validateForm() {
         var isValid = true;
-<<<<<<< HEAD
-        $('.chosen-select').each(function () {
-=======
      
       $('.chosen-select').each(function () {
->>>>>>> 74e3884585675795747ce94597c1868c178dd207
             if ($(this).val() === "")
                 isValid = false
         })
@@ -26,15 +22,6 @@ $("#submit").on("click", function () {
             
         }
 
-<<<<<<< HEAD
-
-
-        $.post("/api/locations/new", userData).done(function (data) {
-
-            $("#matchLocation").text(data.name);
-            $("#resultsModal").modal('toggle');
-        });
-=======
         $.post("/api/locations", surveyData).done (function(data){
            
          
@@ -42,7 +29,6 @@ $("#submit").on("click", function () {
                 location.reload();
                 console.log(data);
             });
->>>>>>> 74e3884585675795747ce94597c1868c178dd207
     }
 
     else {
