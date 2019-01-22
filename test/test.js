f3d8cf4611d0889ec4d484dd2c4e84227863511e
Calum Ragan <searagan@gmail.com>
2019-01-22 09:44:22 -0800

    master hand

--- test/test.js
+++ test/test.js
@@ -11,14 +11,14 @@ describe('Load a page', function () {
     describe('/ (Home Page)', () => {
         it('should load without error', done => {
             nightmare
-            .goto('http://localhost:8080/')
+            .goto('https://raft-travel.herokuapp.com')
             .on('page', (type, message) => {
                 if (type == 'alert') done()
               })
               .type('#email-input', 'searagan@gmail.com')
               .type('#password-input', 'alvin')
               .click('#login-submit')
-              .click('#raft-list')
+              .click('#raft-survey')
               .wait(2000)
               .end()
               .then()
